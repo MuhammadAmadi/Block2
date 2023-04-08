dc144a3f918d7e5e067b55f6bb5be18385102d9b
Muhammad <type.92@mail.ru>
2023-04-08 13:46:14 +0300

    end

---
+++ Python/Seminars/Seminar1/main.py
@@ -0,0 +1,80 @@
+# a = int(input('Введите первое число'))
+# b = int(input('Введите второе число'))
+# c = int(input('Введите третье число'))
+#
+# sum = a+b+c
+#
+# print(sum)
+import math
+
+# a = int(input('Введите сторону A'))
+# b = int(input('Введите сторону B'))
+# p = a*b
+#
+# print(p)
+
+# a = int(input('Введите число A'))
+# b = int(input('Введите число B'))
+#
+# c = a%2==0 and b%2==0
+# if c:
+#     print(f"Чилса {a} и {b} имеют одинаковую четн6ость")
+# else:
+#
+#     print(f"Чилса {a} и {b} имеют одинаковую четн6ость")
+
+# a = int(input('Введите число A '))
+# b = int(input('Введите число B '))
+# c = int(input('Введите число C '))
+#
+# d = a > 0 or b > 0 or c > 0
+# if d:
+#     print("Минимум 1 число из введенных положительное")
+# else:
+#     print("Все числа отрицательные")
+
+# a = int(input('Введите число A'))
+# b = int(input('Введите число B'))
+#
+# if a > b:
+#     print(a)
+# elif a ==b:
+#     print("равны")
+# else:
+#     print(b)
+
+# a = int(input('Введите число A'))
+#
+# if a < 0:
+#     a = a*-1
+#
+# if a>=1000 and a <= 9999:
+#     print('YES')
+# else:
+#     print('NO')
+
+# a = int(input('Введите число A'))
+# b = int(input('Введите число B'))
+# c = math.ceil(b / a)
+#
+# print(c)
+
+# def vag():
+#     a = int(input('введите номер вагона, в который сел Витя'))
+#     b = int(input('введите фактический номер вагона'))
+#     res = b + a - 1
+#     return res
+#
+# print(vag())
+
+# def vag():
+#     a = int(input('введите год'))
+#     res = 'NO'
+#     if a % 2 == 0 and a % 100 != 100:
+#         res = 'YES'
+#     if a % 400 == 0:
+#         res = 'YES'
+#
+#     return res
+#
+# print(vag())
