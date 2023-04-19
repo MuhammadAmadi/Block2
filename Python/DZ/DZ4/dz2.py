dcbb5d31423ec5076bcdc2dc7bde6702cc54775a
Muhammad <type.92@mail.ru>
2023-04-20 00:01:49 +0300

    dz2.3

--- Python/DZ/DZ4/dz2.py
+++ Python/DZ/DZ4/dz2.py
@@ -16,16 +16,20 @@ list_cust = []
 for i in range(n):
     # list_cust.append(random.randint(1, n*2))
     list_cust.append(int(input("Введите значение")))
-
+temp_val = "Длина листа короче шага"
 print(list_cust)
 step = 3
-val = temp_val = sum(list_cust[:step])
-for i in range(n-1):
-    j = (i + step) % n
-    val = val - list_cust[i] + list_cust[j]
+if len(list_cust) > step:
+    val = temp_val = sum(list_cust[:step])
 
-    if val > temp_val:
-        temp_val = val
+    for i in range(n-1):
+        j = (i + step) % n
+        val = val - list_cust[i] + list_cust[j]
 
+        if val > temp_val:
+            temp_val = val
+
+elif len(list_cust) == step:
+    temp_val = sum(list_cust[:step])
 
 print(temp_val)
