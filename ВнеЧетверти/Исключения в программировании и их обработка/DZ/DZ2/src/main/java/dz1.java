bf0cb19914617f04377b3d798ee849a7c46357b8
Muhammad <type.92@mail.ru>
2023-04-22 21:35:30 +0300

    DZ2

---
+++ ВнеЧетверти/Исключения в программировании и их обработка/DZ/DZ2/src/main/java/dz1.java
@@ -0,0 +1,44 @@
+//Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение.
+//Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить
+//у пользователя ввод данных.
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class dz1 {
+//    public static float method2(){
+//        Scanner in = new Scanner(System.in);
+//        float digit;
+//        while (true){
+//            System.out.println("Введите дробное число");
+//            if (in.hasNextFloat()){
+//                digit = Float.parseFloat(scanner.nextLine());
+//                break;
+//            }
+//            else {
+//                System.out.println("неправильный ввод");
+//                in.nextLine();
+//            }
+//        }
+//        return digit;
+//    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float digit;
+        while (true){
+            try {
+                System.out.println("Введите дробное число");
+                digit = in.nextFloat();
+
+            }catch (InputMismatchException e){
+                System.out.println("неправильный ввод");
+                in.nextLine();
+                continue;
+            }
+            break;
+        }
+        System.out.println("Вы ввели");
+        System.out.println(digit);
+    }
+}
