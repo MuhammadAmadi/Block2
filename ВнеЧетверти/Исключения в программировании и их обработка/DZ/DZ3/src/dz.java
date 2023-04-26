2942a6d3c2ac5ff6f62b786e7b7f879d160777ad
Muhammad <type.92@mail.ru>
2023-04-27 01:46:46 +0300

    DZ3.3

--- ВнеЧетверти/Исключения в программировании и их обработка/DZ/DZ3/src/dz.java
+++ ВнеЧетверти/Исключения в программировании и их обработка/DZ/DZ3/src/dz.java
@@ -6,12 +6,12 @@ public class dz {
     public static void main(String[] args) {
         String lastName = null;
         try(Scanner in = new Scanner(System.in)) {
-            System.out.println("Введите ФИО, дата рождения в формате dd.mm.yyy, номер телефона только цифры без знаков" +
+            System.out.println("Введите ФИО, дата рождения в формате dd.mm.yyyy, номер телефона только цифры без знаков" +
                     "до 13 цифр, пол f-женский m-мужской");
             if (in.hasNext("[A-Za-zА-Яа-я0-9]+"))
                 lastName = MyHumanDataBase.myDataParse(in.nextLine());
             else
-                throw new MyInvalidInputFormat(in.nextLine().replaceAll("[A-Za-zА-Яа-я]*", ""));
+                throw new MyInvalidInputFormat("Неверный"+in.nextLine().replaceAll("[A-Za-zА-Яа-я0-9]*", ""));
         }catch (MyInvalidInputFormat e){
             e.printMessage();
         }
