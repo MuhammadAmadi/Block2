2fc1c66e16d0110aa75937864381be07040a0ec6
Muhammad <type.92@mail.ru>
2023-04-26 13:37:03 +0300

    DZ3.1

--- ВнеЧетверти/Исключения в программировании и их обработка/DZ/DZ3/src/MyHumanDataBase.java
+++ ВнеЧетверти/Исключения в программировании и их обработка/DZ/DZ3/src/MyHumanDataBase.java
@@ -1,11 +1,12 @@
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class MyHumanDataBase {
 
     public static String myDataParse (String name, String last_name){
-        boolean check = true;
-        if (last_name == null) check = false;
+        String [] myData = name.split(" ");
+        if (myData.length != 6)  throw new MyInvalidInputFormat("Неверный ввод данных нужно ввести 6 элементов вы ввели "
+                + myData.length + "последний элемент " + myData[myData.length-1]);
+        boolean check = last_name != null;
         for (String i : name.split(" ")){
             if (Pattern.matches("[a-zA-Z]+",i)){
                 if (check && last_name == i){
