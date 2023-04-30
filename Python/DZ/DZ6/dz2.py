303498345ba74ac3ccb5201d1d0115cd08fe81a5
Muhammad <type.92@mail.ru>
2023-05-01 00:47:22 +0300

    dz6.1

--- Python/DZ/DZ6/dz2.py
+++ Python/DZ/DZ6/dz2.py
@@ -6,4 +6,3 @@ my_rand_list = [random.randint(-20, 50) for i in range(random.randint(10, 40))]
 print(my_rand_list)
 first, last = map(int, input("Введите диапазон чисел через пробел например -20 50 ").split())
 print(" ".join([str(i) for i in range(len(my_rand_list)) if first <= my_rand_list[i] <= last]))
-
